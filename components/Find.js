5008e27b134637673160e49a67e368c57c0e742a
Maazasim1 <mazzasimq1@gmail.com>
2022-09-10 22:25:33 +0500

    fixed production bugs

--- components/Find.js
+++ components/Find.js
@@ -36,7 +36,7 @@ export default function Find(props) {
           <select onChange={(e)=>changeRow(e.target.value)} id="dropdown-button-2" data-dropdown-toggle="dropdown-search-city" className="flex-shrink-0 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-500 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100" type="button">
             {
               props.data[0] && Object.keys(props.data[0])?.map((options) => (
-                <option value={options}>
+                <option key={indexOf(options)} value={options}>
                   {options}
                 </option>
               ))
@@ -44,7 +44,7 @@ export default function Find(props) {
           </select>
           <input ref={searchRef} type='search' id="location-search" className="block p-2.5 w-full  text-sm text-buttonWhite bg-backWhite rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder='Search' />
           <button className="p-2.5 text-sm font-medium text-white bg-Orange rounded-r-lg border border-x-Orange focus:ring-4 focus:outline-none focus:ring-blue-300" type='submit'>
-            <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             <span className="sr-only">Search</span>
           </button>
         </div>
