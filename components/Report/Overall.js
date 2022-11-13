193faa3ca9629499a46dff8b7d84e748c053ebd4
Maaz Asim <58209878+Maazasim1@users.noreply.github.com>
2022-11-13 22:53:46 +0000

    fixed bugs

---
+++ components/Report/Overall.js
@@ -0,0 +1,390 @@
+export default function OverAll() {
+    return (
+      
+      <>
+          <div className='ml-[35vw]'>
+  
+  <div className='p-20 bg-white h-[297mm] w-[210mm] text-headBlu rounded-lg' id='reportComponent'>
+      <h1 className='flex justify-center pt-10'>CRUDE RECEIPT & PRODUCT SHIPMENT REPORT</h1>
+      <h4 className='flex justify-center p-5'>DATE</h4>
+      <h5 className='p-5'>Current Date</h5>
+      <div  className="overflow-x-hidden relative shadow-md sm:rounded-lg">
+  
+      <table className='w-full text-sm text-left text-buttonWhite'>
+          <thead className='text-xs text-white uppercase bg-Orange'>
+              <tr>
+                  <th scope="col" className="py-3 px-6">Customers</th>
+                  <th scope="col" className="py-3 px-6">FO</th>
+                  <th scope="col" className="py-3 px-6">HSD</th>
+                  <th scope="col" className="py-3 px-6">PMG</th>
+                  <th scope="col" className="py-3 px-6">JP-8</th>
+                  <th scope="col" className="py-3 px-6">LDO</th>
+                  <th scope="col" className="py-3 px-6">SKO</th>
+                  <th scope="col" className="py-3 px-6">HOBC</th>
+                  <th scope="col" className="py-3 px-6">LPG</th>
+                  <th scope="col" className="py-3 px-6">Total</th>
+             
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+              <th scope="row" className="py-4 px-6 font-medium text-Orange whitespace-nowrap">
+                                  VTPQ
+                              </th>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+              </tr>
+              <tr>
+              <th scope="row" className="py-4 px-6 font-medium text-Orange whitespace-nowrap">
+                                  VTPQ
+                              </th>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+              </tr>
+              <tr>
+              <th scope="row" className="py-4 px-6 font-medium text-Orange whitespace-nowrap">
+                                  VTPQ
+                              </th>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+              </tr>
+              <tr>
+              <th scope="row" className="py-4 px-6 font-medium text-Orange whitespace-nowrap">
+                                  VTPQ
+                              </th>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+              </tr>
+              <tr>
+              <th scope="row" className="py-4 px-6 font-medium text-Orange whitespace-nowrap">
+                                  VTPQ
+                              </th>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+              </tr>
+              <tr>
+              <th scope="row" className="py-4 px-6 font-medium text-Orange whitespace-nowrap">
+                                  VTPQ
+                              </th>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+              </tr>
+              <tr>
+              <th scope="row" className="py-4 px-6 font-medium text-Orange whitespace-nowrap">
+                                  VTPQ
+                              </th>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  -
+                              </td>
+                              <td className="py-4 px-6">
+                                  2406
+                              </td>
+              </tr>
+              <tr>
+              <th scope="row" className="py-4 px-6 font-medium text-white bg-Orange whitespace-nowrap">
+              TOTAL KLs DISPATCHED
+                              </th>
+                              <td className="py-4 px-6 text-white bg-Orange">
+                                  2726
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  3568
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  2246
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  94
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  8634
+                              </td>
+              </tr>
+              <tr>
+              <th scope="row" className="py-4 px-6 font-medium text-white bg-Orange whitespace-nowrap">
+              TOTAL Obs BBLS
+                              </th>
+                              <td className="py-4 px-6 text-white bg-Orange">
+                                  2726
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  3568
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  2246
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  94
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  8634
+                              </td>
+              </tr>
+              <tr>
+              <th scope="row" className="py-4 px-6 font-medium text-white bg-Orange whitespace-nowrap">
+              TOTAL M.Ton
+                              </th>
+                              <td className="py-4 px-6 text-white bg-Orange">
+                                  2726
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  3568
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  2246
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  94
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  8634
+                              </td>
+              </tr>
+              <tr>
+              <th scope="row" className="py-4 px-6 font-medium text-white bg-Orange whitespace-nowrap">
+              TOTAL TLs DISPATCHED
+                              </th>
+                              <td className="py-4 px-6 text-white bg-Orange">
+                                  2726
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  3568
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  2246
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  -
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  94
+                              </td>
+                              <td className="py-4 px-6  text-white bg-Orange">
+                                  8634
+                              </td>
+              </tr>
+          </tbody>
+      </table>
+      </div>
+  </div>
+  </div>
+  
+      </>
+    )
+  }
