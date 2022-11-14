be30e2991c12d5918a720180dae482af087ecfb1
Maaz Asim <58209878+Maazasim1@users.noreply.github.com>
2022-11-14 01:19:05 +0000

    added report fratures

--- components/Report/ReportStructure.js
+++ components/Report/ReportStructure.js
@@ -4,36 +4,84 @@ import OverAll from './Overall'
 
 
 export default function ReportStructure() {
-    const [overAll, setOverAll] = useState(true)
-    const [bowswerSummary, setBowswerSummary] = useState(false)
-    const [statusRefinery, setStatusRefinery] = useState(false)
-    const [tankGauge, setTankGauge] = useState(false)
-    const [loadingPlanvsShip, setLoadingPlanvsShip] = useState(false)
-    const [partyWiseShip, setPartyWiseShip] = useState(false)
-    const [quickStatus, setQuickStatus] = useState(false)
-    const [dropDownList, setDropDownList] = useState(false)
-    
+  const [overAll, setOverAll] = useState(true)
+  const [bowswerSummary, setBowswerSummary] = useState(false)
+  const [statusRefinery, setStatusRefinery] = useState(false)
+  const [tankGauge, setTankGauge] = useState(false)
+  const [loadingPlanvsShip, setLoadingPlanvsShip] = useState(false)
+  const [partyWiseShip, setPartyWiseShip] = useState(false)
+  const [quickStatus, setQuickStatus] = useState(false)
+  const [dropDownList, setDropDownList] = useState(false)
+
+  
+  const handlerReports = (event) => {
+    const { name, value } = event.target
+    console.log(name, value)
+    setOverAll(false)
+    setBowswerSummary(false)
+    setStatusRefinery(false)
+    setTankGauge(false)
+    setLoadingPlanvsShip(false)
+    setPartyWiseShip(false)
+    setQuickStatus(false)
+    setDropDownList(false)
+    if (name === 'overAll') {
+      setOverAll(true)
+    }
+    if (name === 'bowswerSummary') {
+      setBowswerSummary(true)
+    }
+    if (name === 'statusRefinery') {
+      setStatusRefinery(true)
+    }
+
+    if (name === 'tankGauge') {
+      setTankGauge(true)
+    }
+    if (name === 'loadingPlanvsShip') {
+      setLoadingPlanvsShip(true)
+    }
+    if (name === 'partyWiseShip') {
+      setPartyWiseShip(true)
+    }
+    if (name === 'quickStatus') {
+      setQuickStatus(true)
+    }
+
+    if (name === 'dropDownList') {
+
+      setDropDownList(true)
+    }
+  }
+
+
+
+
+
+
+
+
   return (
     <>
-    <div className='flex justify-center '>
+      <div className='flex justify-center '>
 
-    <nav className=' ml-[190PX] text-white bg-Orange rounded-lg p-5 my-10 w-[1200px] '>
-        
-            <button onClick={()=>setOverAll(!overAll)}>Over All</button>
-            <button className='px-3' onClick={()=>setBowswerSummary(!bowswerSummary)}>Bowswer Summary</button>
-            <button className='px-3' onClick={()=>setStatusRefinery(!statusRefinery)}>Status Refinery</button>
-            <button className='px-3' onClick={()=>setTankGauge(!tankGauge)}>Tank Gauge</button>
-            <button className='px-3' onClick={()=>setLoadingPlanvsShip(!loadingPlanvsShip)}>Loading Plan vs Ship</button>
-            <button className='px-3' onClick={()=>setPartyWiseShip(!partyWiseShip)}>Party Wise Ship</button>
-            <button className='px-3' onClick={()=>setQuickStatus(!quickStatus)}>Quick Status</button>
-            <button className='px-3' onClick={()=>setDropDownList(!dropDownList)}>Drop Down List</button>
-            
-            
-            
-    </nav>
-    </div>
-     {overAll ? <OverAll /> : null}
-     
+        <nav className=' ml-[190PX] text-white bg-Orange rounded-lg p-5 my-10 w-[1200px] '>
+
+          <button name='overAll' className='px-6' onClick={() => handlerReports(event)}>Over All</button>
+          <button name='bowswerSummary' className='px-6' onClick={() => handlerReports(event)}>Bowswer Summary</button>
+          <button name='statusRefinery' className='px-6' onClick={() => handlerReports(event)}>Status Refinery</button>
+          <button name='tankGauge' className='px-6' onClick={() => handlerReports(event)}>Tank Gauge</button>
+          <button name='loadingPlanvsShip' className='px-6' onClick={() => handlerReports(event)}>Loading Plan vs Ship</button>
+          <button name='partyWiseShip' className='px-6' onClick={() => handlerReports(event)}>Party Wise Ship</button>
+          <button name='quickStatus' className='px-6' onClick={() => handlerReports(event)}>Quick Status</button>
+          <button name='dropDownList' className='px-6' onClick={() => handlerReports(event)}>Drop Down List</button>
+
+
+
+        </nav>
+      </div>
+      {overAll ? <OverAll /> : null}
+
 
     </>
   )
