1ce9174ea53429694a1e334e383ba701081bfa2b
Maaz Asim <58209878+Maazasim1@users.noreply.github.com>
2022-11-14 01:18:48 +0000

    added new report features

--- components/Report/ReportStructure.js
+++ components/Report/ReportStructure.js
@@ -2,29 +2,38 @@ import React from 'react'
 import { useState } from 'react'
 import OverAll from './Overall'
 
-const selectedReport={
-    overAll: false,
-    bowswerSummary: false,
-    statusRefinery: false,
-    tankGauge: false,
-    loadingPlanvsShip: false,
-    partyWiseShip: false,
-    quickStatus: false,
-    dropDownList: false,
-}
 
 export default function ReportStructure() {
-const [report, setReport] = useState(selectedReport)
-
+    const [overAll, setOverAll] = useState(true)
+    const [bowswerSummary, setBowswerSummary] = useState(false)
+    const [statusRefinery, setStatusRefinery] = useState(false)
+    const [tankGauge, setTankGauge] = useState(false)
+    const [loadingPlanvsShip, setLoadingPlanvsShip] = useState(false)
+    const [partyWiseShip, setPartyWiseShip] = useState(false)
+    const [quickStatus, setQuickStatus] = useState(false)
+    const [dropDownList, setDropDownList] = useState(false)
+    
   return (
     <>
-    <nav>
-        <ul>
-            <li onClick={setReport({overAll:!report.overAll})}>Over All</li>
-        </ul>
+    <div className='flex justify-center '>
+
+    <nav className=' ml-[190PX] text-white bg-Orange rounded-lg p-5 my-10 w-[1200px] '>
+        
+            <button onClick={()=>setOverAll(!overAll)}>Over All</button>
+            <button className='px-3' onClick={()=>setBowswerSummary(!bowswerSummary)}>Bowswer Summary</button>
+            <button className='px-3' onClick={()=>setStatusRefinery(!statusRefinery)}>Status Refinery</button>
+            <button className='px-3' onClick={()=>setTankGauge(!tankGauge)}>Tank Gauge</button>
+            <button className='px-3' onClick={()=>setLoadingPlanvsShip(!loadingPlanvsShip)}>Loading Plan vs Ship</button>
+            <button className='px-3' onClick={()=>setPartyWiseShip(!partyWiseShip)}>Party Wise Ship</button>
+            <button className='px-3' onClick={()=>setQuickStatus(!quickStatus)}>Quick Status</button>
+            <button className='px-3' onClick={()=>setDropDownList(!dropDownList)}>Drop Down List</button>
+            
+            
             
     </nav>
-     {report.overAll ? <OverAll /> : null}
+    </div>
+     {overAll ? <OverAll /> : null}
+     
 
     </>
   )
